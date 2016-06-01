1b4031003e74469c3a8673b860f832c403882d31
niyn <niyn>
2016-06-01 18:06:19 +0800

    改动

--- src/main/java/com/crec/data/DataService.java
+++ src/main/java/com/crec/data/DataService.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Arrays;
 
 import com.crec.bean.MDMMATCOM;
 import com.crec.bean.MDMMATERIALS;
@@ -12,27 +13,34 @@ import com.crec.bean.MDMUNITS;
 import com.crec.conn.ConnectionPool;
 
 public class DataService {
-	public static boolean add_mdmunits(MDMUNITS[] data,String table) {
+	public static boolean add_mdmunitsByCount(MDMUNITS[] data,String table) {
 		String insert = "insert into "+table+"(UNIT_ID,UNITNM,SSNM,RECTYPE,RECNOTE,RECCREATETIME,RECUPDATETIME,RECCREATEEMPNM,RECUPDATEEMPNM) values(?,?,?,?,?,?,?,?,?)";
 		ConnectionPool pool = ConnectionPool.getInstance();
 		Connection conn = pool.getConnection();
 		try {
 			PreparedStatement p_insert = conn.prepareStatement(insert);
 			conn.setAutoCommit(false);
-			for (MDMUNITS d : data){
-				p_insert.setString(1,d.getUnit_id());
-				p_insert.setString(2, d.getUnitnm());
-				p_insert.setString(3, d.getSsnm());
-				p_insert.setString(4, d.getRectype());
-				p_insert.setString(5, d.getRecnote());
-				p_insert.setDate(6, new Date( d.getRECCREATETIME().getTime()));
-				p_insert.setDate(7,new Date(d.getRECUPDATETIME().getTime()) );
-				p_insert.setString(8, "LBMDM");
-				p_insert.setString(9, "LBMDM");
-				p_insert.addBatch();
+			int count = data.length;
+	    	for (int i=1,k=0; i <= count; i++) {
+				if(i%1000==0 || i==count){
+					for (MDMUNITS d : Arrays.asList(data).subList(1000*k, i)){
+						p_insert.setString(1,d.getUnit_id());
+						p_insert.setString(2, d.getUnitnm());
+						p_insert.setString(3, d.getSsnm());
+						p_insert.setString(4, d.getRectype());
+						p_insert.setString(5, d.getRecnote());
+						p_insert.setDate(6, new Date( d.getRECCREATETIME().getTime()));
+						p_insert.setDate(7,new Date(d.getRECUPDATETIME().getTime()) );
+						p_insert.setString(8, "LBMDM");
+						p_insert.setString(9, "LBMDM");
+						p_insert.addBatch();
+					}
+					p_insert.executeBatch();
+					conn.commit();
+					k++;
+					System.out.println(k);
+				}
 			}
-			p_insert.executeBatch();
-			conn.commit();
 			conn.setAutoCommit(true);
 			return true;
 		} catch (Exception e) {
@@ -47,7 +55,8 @@ public class DataService {
 		return false;
 	}
 
-	public static boolean add_mdmmatsorts(MDMMATSORTS[] data,String table) {
+	
+	public static boolean add_mdmmatsortsByCount(MDMMATSORTS[] data,String table) {
 		String insert = "insert into "+table+"(CLASSCD,CLASSNM,PARENTCLASSCD,"
 				+ "CLASSLEVEL,RECTYPE,RECNOTE,SPESORTS,CLASS_ID,RECCREATETIME,RECUPDATETIME,RECCREATEEMPNM,RECUPDATEEMPNM)"
 				+ " values(?,?,?,?,?,?,?,?,?,?,?,?)";
@@ -56,23 +65,30 @@ public class DataService {
 		try {
 			PreparedStatement p_insert = conn.prepareStatement(insert);
 			conn.setAutoCommit(false);
-			for (MDMMATSORTS d : data) {
-				p_insert.setString(1, d.getClasscd());
-				p_insert.setString(2, d.getClassnm());
-				p_insert.setString(3, d.getParentclasscd());
-				p_insert.setString(4, d.getClasslevel());
-				p_insert.setString(5, d.getRectype());
-				p_insert.setString(6, d.getRecnote());
-				p_insert.setString(7, d.getSpesorts());
-				p_insert.setString(8, d.getClass_id());
-				p_insert.setDate(9,  new Date(d.getReccreatetime().getTime()) );
-				p_insert.setDate(10, new Date(d.getRecupdatetime().getTime()));
-				p_insert.setString(11, "LBMDM");
-				p_insert.setString(12, "LBMDM");
-				p_insert.addBatch();
+			int count = data.length;
+			for (int i=1,k=0; i <= count; i++) {
+				if(i%1000==0 || i==count){
+				for (MDMMATSORTS d : Arrays.asList(data).subList(1000*k, i)){
+						p_insert.setString(1, d.getClasscd());
+						p_insert.setString(2, d.getClassnm());
+						p_insert.setString(3, d.getParentclasscd());
+						p_insert.setString(4, d.getClasslevel());
+						p_insert.setString(5, d.getRectype());
+						p_insert.setString(6, d.getRecnote());
+						p_insert.setString(7, d.getSpesorts());
+						p_insert.setString(8, d.getClass_id());
+						p_insert.setDate(9,  new Date(d.getReccreatetime().getTime()) );
+						p_insert.setDate(10, new Date(d.getRecupdatetime().getTime()));
+						p_insert.setString(11, "LBMDM");
+						p_insert.setString(12, "LBMDM");
+						p_insert.addBatch();
+						}
+						p_insert.executeBatch();
+						conn.commit();
+						k++;
+						System.out.println(k);
+					}
 			}
-			p_insert.executeBatch();
-			conn.commit();
 			conn.setAutoCommit(true);
 			return true;
 		} catch (Exception e) {
@@ -86,11 +102,9 @@ public class DataService {
 		}
 		return false;
 	}
-
-//	SPESORTS,MATERIALNM,CompanyMatCode ,SPECS,MATERIALCZ,DRAWNO,PRODDETAILS,ITEMNAME,
-//	MATERIALSUCHEN,MATERIALSORTNM,PRIMARYUNITNM,STOPFLAG,EDITSTATE,CREATETIME,LASTMODIFIEDTIME,OLDCODE
 	
-	public static boolean add_mdmmaterials(MDMMATERIALS[] data,String table) {
+	
+	public static boolean add_mdmmaterialsByCount(MDMMATERIALS[] data,String table) {
 		String insert = "insert into "+table+"(SPESORTS,PRODUCTCD,PRODDETAILS,PRODUCTNM,PRODUCTSNM,PRODUCTMODEL,"
 				+ "PRODUCTMATERIAL,PRODUCTTH,RECTYPE,RECNOTE,"
 				+ "PRODUCT_IN_ID,CLASS_ID,UNIT_IN_ID,RECCREATETIME,RECUPDATETIME,RECCREATEEMPNM,RECUPDATEEMPNM)"
@@ -100,28 +114,36 @@ public class DataService {
 		try {
 			PreparedStatement p_insert = conn.prepareStatement(insert);
 			conn.setAutoCommit(false);
-			for (MDMMATERIALS d : data) {
-				p_insert.setString(1, d.getSPESORTS());
-				p_insert.setString(2, d.getProductcd());
-				p_insert.setString(3, d.getProddetails());
-				p_insert.setString(4, d.getProductnm());
-				p_insert.setString(5, d.getProductsnm());
-				p_insert.setString(6, d.getProductmodel());
-				p_insert.setString(7, d.getProductmaterial());
-				p_insert.setString(8, d.getProductth());
-				p_insert.setString(9, d.getRectype());
-				p_insert.setString(10, d.getRecnote());
-				p_insert.setString(11, d.getProduct_in_id());
-				p_insert.setString(12, d.getClass_id());
-				p_insert.setString(13, d.getUnit_id());
-				p_insert.setDate(14, new Date(d.getRECCREATETIME().getTime()));
-				p_insert.setDate(15, new Date(d.getRECUPDATETIME().getTime()));
-				p_insert.setString(16,"LBMDM");
-				p_insert.setString(17, "LBMDM");
-				p_insert.addBatch();
+			int count = data.length;
+			for (int i=1,k=0; i <= count; i++) {
+				if(i%1000==0 || i==count){
+				for (MDMMATERIALS d : Arrays.asList(data).subList(1000*k, i)){
+					p_insert.setString(1, d.getSPESORTS());
+					p_insert.setString(2, d.getProductcd());
+					p_insert.setString(3, d.getProddetails());
+					p_insert.setString(4, d.getProductnm());
+					p_insert.setString(5, d.getProductsnm());
+					p_insert.setString(6, d.getProductmodel());
+					p_insert.setString(7, d.getProductmaterial());
+					p_insert.setString(8, d.getProductth());
+					p_insert.setString(9, d.getRectype());
+					p_insert.setString(10, d.getRecnote());
+					p_insert.setString(11, d.getProduct_in_id());
+					p_insert.setString(12, d.getClass_id());
+					p_insert.setString(13, d.getUnit_id());
+					p_insert.setDate(14, new Date(d.getRECCREATETIME().getTime()));
+					p_insert.setDate(15, new Date(d.getRECUPDATETIME().getTime()));
+					p_insert.setString(16,"LBMDM");
+					p_insert.setString(17, "LBMDM");
+					p_insert.addBatch();
+				  }
+					p_insert.executeBatch();
+					conn.commit();
+					k++;
+					System.out.println(k);
+				}
 			}
-			p_insert.executeBatch();
-			conn.commit();
+			
 			conn.setAutoCommit(true);
 			return true;
 		} catch (Exception e) {
@@ -136,7 +158,7 @@ public class DataService {
 		return false;
 	}
 	
-	public static boolean add_mdmmatcom(MDMMATCOM[] data,String table) {
+	public static boolean add_mdmmatcomByCount(MDMMATCOM[] data,String table) {
 		String insert = "insert into "+table+"(PRODUCT_ID,CLASS_ID,RECTYPE,SPESORTS,"
 				+ "RECCREATETIME,RECUPDATETIME,RECCREATEEMPNM,RECUPDATEEMPNM)"
 				+ " values(?,?,?,?,?,?,?,?)";
@@ -145,19 +167,26 @@ public class DataService {
 		try {
 			PreparedStatement p_insert = conn.prepareStatement(insert);
 			conn.setAutoCommit(false);
-			for (MDMMATCOM d : data) {
-				p_insert.setString(1, d.getProduct_id());
-				p_insert.setString(2, d.getClass_id());
-				p_insert.setString(3, d.getRectype());
-				p_insert.setString(4, d.getSpesorts());
-				p_insert.setDate(5, new Date(d.getRECCREATETIME().getTime()) );
-				p_insert.setDate(6, new  Date(d.getRECUPDATETIME().getTime()));
-				p_insert.setString(7, "LBMDM");
-				p_insert.setString(8, "LBMDM");
-				p_insert.addBatch();
+			int count = data.length;
+			for (int i=1,k=0; i <= count; i++) {
+				if(i%1000==0 || i==count){
+				for (MDMMATCOM d : Arrays.asList(data).subList(1000*k, i)){
+					p_insert.setString(1, d.getProduct_id());
+					p_insert.setString(2, d.getClass_id());
+					p_insert.setString(3, d.getRectype());
+					p_insert.setString(4, d.getSpesorts());
+					p_insert.setDate(5, new Date(d.getRECCREATETIME().getTime()) );
+					p_insert.setDate(6, new  Date(d.getRECUPDATETIME().getTime()));
+					p_insert.setString(7, "LBMDM");
+					p_insert.setString(8, "LBMDM");
+					p_insert.addBatch();
+					}
+					p_insert.executeBatch();
+					conn.commit();
+					k++;
+					System.out.println(k);
+				}
 			}
-			p_insert.executeBatch();
-			conn.commit();
 			conn.setAutoCommit(true);
 			return true;
 		} catch (Exception e) {
--- src/main/java/com/crec/data/IMP.java
+++ src/main/java/com/crec/data/IMP.java
@@ -16,30 +16,30 @@ public class IMP {
 		CsvReader r = null;
 		List<MDMMATERIALS> datas = new ArrayList<MDMMATERIALS>();
 		try {
-			r = new CsvReader(path, ',', Charset.forName("GBK"));
+			r = new CsvReader(path, ',', Charset.forName("UTF-8"));
 			r.readHeaders();
 //			SPESORTS,MATERIALNM,CompanyMatCode ,SPECS,PRODDETAILS,ITEMNAME,MATERIALSUCHEN,MATERIALSORTNM,
 //			PRIMARYUNITNM,STOPFLAG,EDITSTATE,CREATETIME,LASTMODIFIEDTIME,RECCREATEEMPNM
 			while (r.readRecord()) {
 				MDMMATERIALS data = new MDMMATERIALS();
-				data.setSPESORTS(r.get("SPESORTS"));
-				data.setProductcd(r.get("CompanyMatCode"));
-				data.setProddetails(r.get("PRODDETAILS"));
-				data.setProductnm(r.get("ITEMNAME"));
-				data.setProductsnm(r.get("MATERIALSUCHEN"));
-				data.setProductmodel(r.get("SPECS"));
-				data.setProductmaterial(r.get("MATERIALCZ"));
-				data.setProductth(r.get("DRAWNO"));
-				data.setRectype(r.get("EDITSTATE"));
-				data.setRecnote(r.get("STOPFLAG"));
-				data.setProduct_in_id(r.get("MATERIALNM"));
-				data.setClass_id(r.get("MATERIALSORTNM"));
-				data.setUnit_id(r.get("PRIMARYUNITNM"));
-				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME")));
-				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME")));
+				data.setSPESORTS(r.get("SPESORTS").trim());
+				data.setProductcd(r.get("CompanyMatCode").trim());
+				data.setProddetails(r.get("PRODDETAILS").trim());
+				data.setProductnm(r.get("ITEMNAME").trim());
+				data.setProductsnm(r.get("MATERIALSUCHEN").trim());
+				data.setProductmodel(r.get("SPECS").trim());
+				data.setProductmaterial(r.get("MATERIALCZ").trim());
+				data.setProductth(r.get("DRAWNO").trim());
+				data.setRectype(r.get("EDITSTATE").trim());
+				data.setRecnote(r.get("STOPFLAG").trim());
+				data.setProduct_in_id(r.get("MATERIALNM").trim());
+				data.setClass_id(r.get("MATERIALSORTNM").trim());
+				data.setUnit_id(r.get("PRIMARYUNITNM").trim());
+				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME").trim()));
+				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME").trim()));
 				datas.add(data);
 			}
-			DataService.add_mdmmaterials(datas.toArray(new MDMMATERIALS[] {}),table);
+			DataService.add_mdmmaterialsByCount(datas.toArray(new MDMMATERIALS[] {}),table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -52,23 +52,23 @@ public class IMP {
 		CsvReader r = null;
 		List<MDMMATSORTS> datas = new ArrayList<MDMMATSORTS>();
 		try {
-			r = new CsvReader(path, ',', Charset.forName("GBK"));
+			r = new CsvReader(path, ',', Charset.forName("UTF-8"));
 			r.readHeaders();
 			while (r.readRecord()) {
 				MDMMATSORTS data = new MDMMATSORTS();
-				data.setClasscd(r.get("MATERIALSORTCODE"));
-				data.setClassnm(r.get("MATERIALSORTNAME"));
-				data.setParentclasscd(r.get("PARENTCODE"));
-				data.setClasslevel(r.get("LAYER"));
-				data.setRectype(r.get("EDITSTATE"));
-				data.setRecnote(r.get("ISHISTORY"));
-				data.setSpesorts(r.get("SPESORTS"));
-				data.setClass_id(r.get("MATERIALSORTNM"));
-				data.setReccreatetime(TimeUtil.parserTime(r.get("CREATETIME")));
-				data.setRecupdatetime(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME")));
+				data.setClasscd(r.get("MATERIALSORTCODE").trim());
+				data.setClassnm(r.get("MATERIALSORTNAME").trim());
+				data.setParentclasscd(r.get("PARENTCODE").trim());
+				data.setClasslevel(r.get("LAYER").trim());
+				data.setRectype(r.get("EDITSTATE").trim());
+				data.setRecnote(r.get("ISHISTORY").trim());
+				data.setSpesorts(r.get("SPESORTS").trim());
+				data.setClass_id(r.get("MATERIALSORTNM").trim());
+				data.setReccreatetime(TimeUtil.parserTime(r.get("CREATETIME").trim()));
+				data.setRecupdatetime(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME").trim()));
 				datas.add(data);
 			}
-			DataService.add_mdmmatsorts(datas.toArray(new MDMMATSORTS[] {}),table);
+			DataService.add_mdmmatsortsByCount(datas.toArray(new MDMMATSORTS[] {}),table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -81,20 +81,20 @@ public class IMP {
 		CsvReader r = null;
 		List<MDMUNITS> datas = new ArrayList<MDMUNITS>();
 		try {
-			r = new CsvReader(path, ',', Charset.forName("GBK"));
+			r = new CsvReader(path, ',', Charset.forName("UTF-8"));
 			r.readHeaders();
 			while (r.readRecord()) {
 				MDMUNITS data = new MDMUNITS();
-				data.setUnit_id(r.get("UNITNM"));
-				data.setUnitnm(r.get("UNITNAME"));
-				data.setSsnm(r.get("SSNM"));
-				data.setRectype(r.get("EDITSTATE"));
-				data.setRecnote(r.get("ISHISTORY"));
-				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME")));
-				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME")));
+				data.setUnit_id(r.get("UNITNM").trim());
+				data.setUnitnm(r.get("UNITNAME").trim());
+				data.setSsnm(r.get("SSNM").trim());
+				data.setRectype(r.get("EDITSTATE").trim());
+				data.setRecnote(r.get("ISHISTORY").trim());
+				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME").trim()));
+				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME").trim()));
 				datas.add(data);
 			}
-			DataService.add_mdmunits(datas.toArray(new MDMUNITS[] {}),table);
+			DataService.add_mdmunitsByCount(datas.toArray(new MDMUNITS[] {}),table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -107,21 +107,21 @@ public class IMP {
 		CsvReader r = null;
 		List<MDMMATCOM> datas = new ArrayList<MDMMATCOM>();
 		try {
-			r = new CsvReader(path, ',', Charset.forName("GBK"));
+			r = new CsvReader(path, ',', Charset.forName("UTF-8"));
 			r.readHeaders();
 			while (r.readRecord()) {
 				MDMMATCOM data = new MDMMATCOM();
-				data.setProduct_id(r.get("MATERIALNM"));
-				data.setClass_id(r.get("MATERIALSORTNM"));
-				data.setRectype(r.get("EDITSTATE"));
-				data.setSpesorts(r.get("SPESORTS"));
-				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME")));
+				data.setProduct_id(r.get("MATERIALNM").trim());
+				data.setClass_id(r.get("MATERIALSORTNM").trim());
+				data.setRectype(r.get("EDITSTATE").trim());
+				data.setSpesorts(r.get("SPESORTS").trim());
+				data.setRECCREATETIME(TimeUtil.parserTime(r.get("CREATETIME").trim()));
 //				System.out.println("第一个时间："+r.get("CREATETIME"));
-				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME")));
+				data.setRECUPDATETIME(TimeUtil.parserTime(r.get("LASTMODIFIEDTIME").trim()));
 //				System.out.println("第2个时间："+r.get("LASTMODIFIEDTIME"));
 				datas.add(data);
 			}
-			DataService.add_mdmmatcom(datas.toArray(new MDMMATCOM[] {}),table);
+			DataService.add_mdmmatcomByCount(datas.toArray(new MDMMATCOM[] {}),table);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -134,23 +134,25 @@ public class IMP {
 		String T_IT_JD_MATERIALCLASSES = "E:/test/data/csv/T_IT_JD_MATERIALCLASSES.csv";
 		String t_it_jd_relation = "E:/test/data/csv/t_it_jd_relation.csv";
 		String t_it_jd_unit = "E:/test/data/csv/t_it_jd_unit.csv";
-		String t_it_yzb_materials = "E:/test/data/yzb/t_it_yzb_materials.csv";
-		String t_it_yzb_relation = "E:/test/data/yzb/t_it_yzb_relation.csv";
-		String T_IT_yzb_MATERIALCLASSES = "E:/test/data/yzb/T_IT_yzb_MATERIALCLASSES.csv";
-		String t_it_yzb_unit = "E:/test/data/yzb/t_it_yzb_unit.csv";
+		String t_it_yzb_materials = "E:/test/data/yzb/t_it_yzb_materials-20160601.csv";
+		String t_it_yzb_relation = "E:/test/data/yzb/t_it_yzb_relation-20160601.csv";
+		String T_IT_yzb_MATERIALCLASSES = "E:/test/data/yzb/T_IT_yzb_MATERIALCLASSES-20160601.csv";
+		String t_it_yzb_unit = "E:/test/data/yzb/t_it_yzb_unit-20160531.csv";
 //		impMDMMATERIALS(t_it_jd_materials);
 //		impMDMMATSORTS(T_IT_JD_MATERIALCLASSES);
 //		impMDMMATCOM(t_it_jd_relation);
 //		impMDMUNITS(t_it_jd_unit);
-		String table = "T_IT_JD_MATERIALS";
+//		String table = "T_IT_JD_UNIT";
+//		String table = "T_IT_JD_MATERIALS";
 //		String table = "T_IT_YZB_RELATION";
-//		String table = "T_IT_YZB_MATERIALS";
+		String table = "T_IT_YZB_MATERIALS";
 //		String table = "T_IT_YZB_MATERIALCLASSES";
 //		String table = "T_IT_YZB_UNIT";
 		impMDMMATERIALS(t_it_yzb_materials,table);
 //		impMDMMATERIALS(t_it_jd_materials,table);
 //		impMDMMATCOM(t_it_yzb_relation,table);
 //		impMDMUNITS(t_it_yzb_unit,table);
+//		impMDMUNITS(t_it_jd_unit,table);
 //		impMDMMATSORTS(T_IT_yzb_MATERIALCLASSES, table);
 	}
 	
